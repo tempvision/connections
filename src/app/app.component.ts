b236dbcbfb7c1b69582125f63a2f0bba25506d35
nydn <soodker@gmail.com>
2023-11-20 22:42:29 +0200

    Check if words' categories match

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,33 +8,89 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'connections';
 
-  words: Array<string> = [
-    "Пица",        // Pizza
-    "Сьомга",      // Salmon
-    "Киноа",       // Quinoa
-    "Манго",       // Mango
-    "Авокадо",     // Avocado
-    "Шоколад",     // Chocolate
-    "Спанак",      // Spinach
-    "Суши",        // Sushi
-    "Боровинки",   // Blueberries
-    "Такос",       // Tacos
-    "Паста",       // Pasta
-    "Диня",        // Watermelon
-    "Пилешко кари",// Chicken curry
-    "Броколи",     // Broccoli
-    "Сладолед",    // Ice cream
-    "Хумус"        // Hummus
-  ];
+  randomizedWords: Array<string> = [];
 
-  currentSelection: Array<number> = [];
+  words: any = {
+    food: {
+      words: ["Пица", "Сьомга", "Киноа", "Манго"],
+      categoryName: "Храни"
+    },
+    cars: {
+      words: ["Тойота", "Тесла", "Ферари", "Хонда"],
+      categoryName: "Автомобили"
+    },
+    places: {
+      words: ["Париж", "Ню Йорк", "Токио", "Сидни"],
+      categoryName: "Места"
+    },
+    instruments: {
+      words: ["Пиано", "Китара", "Цигулка", "Тромпет"],
+      categoryName: "Инструменти"
+    }
+  };
+
+
+  currentSelection: Array<string> = [];
 
   ngOnInit(): void {
-
+    this.randomizedWords = this.shuffleArray(Object.values(this.words).reduce((acc: any, category: any) => acc.concat(category.words), []));
   }
 
-  selectWord(word: string, wordIndex: number) {
-    this.currentSelection.indexOf(wordIndex) !== -1 ?  this.currentSelection.splice(this.currentSelection.indexOf(wordIndex), 1) : this.currentSelection.push(wordIndex);
-    console.log(this.currentSelection)
+  shuffleArray(array: any) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   }
+
+  selectWord(word: string) {
+    if (this.currentSelection.length === 4 && !this.currentSelection.includes(word)) {
+      return;
+    }
+
+    if (this.currentSelection.includes(word)) {
+      // If the word is already selected, remove it
+      this.currentSelection = this.currentSelection.filter(selectedWord => selectedWord !== word);
+    } else {
+      // If the word is not selected, add it
+      this.currentSelection.push(word);
+    }
+
+    if (this.currentSelection.length > 1) {
+      const result = this.checkCategories(this.currentSelection, this.words);
+
+      if (result) {
+        console.log("All selected words are from the same category!");
+      } else {
+        console.log("Selected words are from different categories or no category!");
+      }
+    }
+  }
+
+  checkCategories(selectedWords: string[], words: any): boolean {
+    if (selectedWords.length === 0) {
+      return false; // No selected words
+    }
+
+    const firstCategory = Object.keys(words).find(category => // get the category of the first word and compare the other words
+      words[category].words.includes(selectedWords[0])
+    );
+
+    if (!firstCategory) {
+      return false; // First word doesn't belong to any category
+    }
+
+    return selectedWords.every(word =>
+      Object.keys(words).some(category =>
+        category === firstCategory && words[category].words.includes(word)
+      )
+    );
+  }
+
+  isWordInCategory(selectedWord: { word: string; category: any }): boolean {
+    const category = this.words[selectedWord.category];
+    return category && category.words.includes(selectedWord.word);
+  }
+
 }
